7345c7ad7d392010b5348da27874fd73e697878e
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-11-14 13:19:59 -0500

    updated closing links

--- src/pages/portfolio-link.js
+++ src/pages/portfolio-link.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function PortfolioLink(props) {
     return (
@@ -8,11 +9,11 @@ function PortfolioLink(props) {
                     <button className="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i className="fas fa-bars"></i></button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav ml-auto">
-                            <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" to="/#portfolio">PORTFOLIO</Link
+                            <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" to="/#portfolio">PORTFOLIO</Link>
                             </li>
-                            <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" to="/#about">ABOUT</Link
+                            <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" to="/#about">ABOUT</Link>
                             </li>
-                            <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" to="/#contact">CONTACT</Link
+                            <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" to="/#contact">CONTACT</Link>
                             </li>
                         </ul>
                     </div>
